9949d9df2a24e8ee6c41c443bde3fe36a9bbacf2
vai na web <vainawebcpx@gmail.com>
2019-12-11 15:45:47 -0300

    imagem adicionada usando ternário

--- src/components/aulaprops.js
+++ src/components/aulaprops.js
@@ -22,6 +22,7 @@ class Properties extends React.Component {
                     <button onClick={() => this.display('azul')}>clickar</button>
                     <button onClick={() => this.display('carro')}>clickar</button>
                     <h1>{this.state.appear}</h1>
+                    {this.state.appear === 'nascar' && <img src= 'https://www.nascar.com/wp-content/uploads/sites/7/2019/01/2019-jan21-team-previews-main-image-625x340.jpg' />}
                 </div>
             )
         }
