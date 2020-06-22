0b69ebcab698736a31eca5517f55f48beed1d20d
vainawebm <vainaweb@gmail.com>
2020-06-22 10:19:21 -0300

    modificações feitas no código e no estilo da página

--- src/components/aulaprops.js
+++ src/components/aulaprops.js
@@ -18,11 +18,13 @@ class Properties extends React.Component {
         render () {
             return (
                 <div>
-                    <button onClick={() => this.display('nascar')}>clickar</button>
-                    <button onClick={() => this.display('azul')}>clickar</button>
-                    <button onClick={() => this.display('carro')}>clickar</button>
-                    <h1>{this.state.appear}</h1>
-                    {this.state.appear === 'nascar' && <img src= 'https://www.nascar.com/wp-content/uploads/sites/7/2019/01/2019-jan21-team-previews-main-image-625x340.jpg' />}
+                    <button className="Click" onClick={() => this.display('nascar')}>clickar</button>
+                    <button className="Click" onClick={() => this.display('azul')}>clickar</button>
+                    <button className="Click" onClick={() => this.display('carro')}>clickar</button>
+                    <h1 className="Title">{this.state.appear}</h1>
+                    {this.state.appear === 'nascar' && <img className="Photo" src= 'https://www.nascar.com/wp-content/uploads/sites/7/2019/01/2019-jan21-team-previews-main-image-625x340.jpg' />}
+                    {this.state.appear === 'azul' && <img className="Photo" src= 'https://www.tuningparts.com.br/blog/wp-content/uploads/2014/05/nascar-3.jpg' />}
+                    {this.state.appear === 'carro' && <img className="Photo" src= 'https://techcrunch.com/wp-content/uploads/2010/02/nascar.jpg' />}
                 </div>
             )
         }
